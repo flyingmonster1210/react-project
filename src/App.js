d636496a21af37ec9a4c06d19431747d1d80c743
Wayne <zweijie1210@outlook.com>
2023-02-21 13:49:27 -0800

    login page initialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Layout from './pages/Layout'
+import Layout from '@/pages/Layout'
+import Login from '@/pages/Login'
 
 function App () {
   return (
-    <div className="App">
-      Learn React
-    </div>
+    // 路由配置
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* 创建对应关系 */}
+          <Route path='/' element={<Layout />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
+import './index.scss'
+
+// 导入antd
+import 'antd/dist/reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
