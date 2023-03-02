e5f4afbe517e61fa5fa024eccf77c4ca24d735b5
Wayne <zweijie1210@outlook.com>
2023-03-02 12:02:34 -0800

    complete the layout of Layout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthComponent } from '@/components/AuthComponent'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
+import './App.css'
 
 function App () {
   return (
